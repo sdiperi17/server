5a5b48109a428b9ec6ff99002f477caf3e97fed8
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-03-24 01:09:51 -0400

    updated the port

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 // tells express to tell node that it wants to listen for incoming traffic 5000
-app.listen(5000);
+app.listen(PORT);
